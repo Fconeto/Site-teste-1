aad8b6f6bb635e11181e61aeea4a5113b127615d
Francisco Neto <linharesipuce@gmail.com>
2021-07-20 12:01:40 -0300

    v.3

---
+++ Área de testes/Position absolute/script.js
@@ -0,0 +1,10 @@
+let uma = document.getElementById('d1');
+let outra = document.getElementById('d2');
+
+function sumir() {
+    uma.style.zIndex = -1;
+}
+
+function aparecer() {
+    uma.style.zIndex = 1;
+}
