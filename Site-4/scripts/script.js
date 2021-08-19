08fbb4404f7d33c93707a4d5d61427fcbe2459a7
Francisco Neto <linharesipuce@gmail.com>
2021-08-19 16:50:43 -0300

    s4.v13

--- Site-4/scripts/script.js
+++ Site-4/scripts/script.js
@@ -1,5 +1,10 @@
 var menu = $(".menu-hamb");
+var main = $(".main");
+var footer = $(".footer");
 
 function openMenu() {
     menu = menu.toggleClass("on");
+    main = main.toggleClass("off");
+    footer = footer.toggleClass("off");
+
 }
